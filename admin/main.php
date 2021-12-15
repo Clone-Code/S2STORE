32bbc9742c361b88633696668a5709605b3fffaa
Clone-Code <53707551+Clone-Code@users.noreply.github.com>
2021-12-15 10:20:36 +0700

    demo

---
+++ admin/main.php
@@ -0,0 +1,131 @@
+<div class="main">
+        <div class="topbar">
+            <div class="toggle">
+                <!-- <ion-icon name="menu-outline"></ion-icon> -->
+            </div>
+            <div class="search">
+                <label for="">
+                    <input type="text" placeholder="tìm kím tại đây">
+                    <ion-icon name="search-outline"></ion-icon>
+                </label>
+            </div>
+            <div class="user">
+                <div class="userName">Simp</div>
+                <img src="user.jpg" alt="">
+            </div>
+        </div>
+        <div  class="cardBox">
+            <div class="card">
+                <div>
+                    <div class="number">1000</div>
+                    <div class="cardName">Lượt Xem</div>
+                </div>
+                <div class="iconBox">
+                    <ion-icon name="eye-outline"></ion-icon>
+                </div>
+            </div>
+            <div class="card">
+                <div>
+                    <div class="number">1000</div>
+                    <div class="cardName">Số Hàng Bán</div>
+                </div>
+                <div class="iconBox">
+                    <ion-icon name="cart-outline"></ion-icon>
+                </div>
+            </div>
+            <div class="card">
+                <div>
+                    <div class="number">1000</div>
+                    <div class="cardName">Lượt Đánh Giá</div>
+                </div>
+                <div class="iconBox">
+                    <ion-icon name="chatbubbles-outline"></ion-icon>
+                </div>
+            </div>
+            <div class="card">
+                <div>
+                    <div class="number">1000</div>
+                    <div class="cardName">Lợi Nhuận</div>
+                </div>
+                <div class="iconBox">
+                    <ion-icon name="cash-outline"></ion-icon>
+                </div>
+            </div>
+        </div>
+        <!-- table -->
+        <div class="details">
+            <div class="productOrders">
+                <div class="cardHeader">
+                    <h2>TOP MẶT HÀNG</h2>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>TÊN</td>
+                            <td>GIÁ(VNĐ)</td>
+                            <td>SỐ LƯỢNG BÁN</td>
+                            <td>TRẠNG THÁI</td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Đồng hồ a</td>
+                            <td>120</td>
+                            <td>100</td>
+                            <td><span class="status0">Hết hàng</span></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Đồng hồ b</td>
+                            <td>120</td>
+                            <td>90</td>
+                            <td><span class="status1">Còn hàng</span></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Đồng hồ c</td>
+                            <td>120</td>
+                            <td>80</td>
+                            <td><span class="status0">Hết hàng</span></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Đồng hồ d</td>
+                            <td>120</td>
+                            <td>70</td>
+                            <td><span class="status1">Còn hàng</span></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>wibu</td>
+                            <td>120</td>
+                            <td>60</td>
+                            <td><span class="status1">Còn hàng</span></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>đồng hồ wibu</td>
+                            <td>120</td>
+                            <td>50</td>
+                            <td><span class="status1">Còn hàng</span></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>đòng hồ e</td>
+                            <td>120</td>
+                            <td>40</td>
+                            <td><span class="status0">Hết hàng</span></td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+            </div>
+        </div>
+</div>
---
+++ admin/sidebar.php
@@ -0,0 +1,54 @@
+<div class="container">
+    <div class="navigation">
+        <ul>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="watch-outline"></ion-icon>
+                    </span>
+                    <span class="title">S2STORE</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="home-outline"></ion-icon>
+                    </span>
+                    <span class="title">Chung</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="person-outline"></ion-icon>
+                    </span>
+                    <span class="title">Người dùng</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="cart-outline"></ion-icon>
+                    </span>
+                    <span class="title">Sản phẩm</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                    </span>
+                    <span class="title">Tin nhắn</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="settings-outline"></ion-icon>
+                    </span>
+                    <span class="title">Cài đặt</span>
+                </a>
+            </li>
+    </div>
+</div>
+
