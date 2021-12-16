ae1fbfe02e498aebc61c76c6f35d38cd50f9df5f
Clone-Code <53707551+Clone-Code@users.noreply.github.com>
2021-12-16 15:10:20 +0700

    demo

---
+++ topbar.php
@@ -0,0 +1,15 @@
+<div class="topbar">
+    <div class="toggle">
+        <!-- <ion-icon name="menu-outline"></ion-icon> -->
+    </div>
+    <div class="search">
+        <label for="">
+            <input type="text" placeholder="tìm kím tại đây">
+            <ion-icon name="search-outline"></ion-icon>
+        </label>
+    </div>
+    <div class="user">
+        <div class="userName">Simp</div>
+        <img src="user.jpg" alt="">
+    </div>
+</div>
