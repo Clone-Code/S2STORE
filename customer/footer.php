62e6889589f168338374743227e719a281564230
Clone-Code <53707551+Clone-Code@users.noreply.github.com>
2021-12-30 11:21:24 +0700

    update

---
+++ customer/footer.php
@@ -0,0 +1,49 @@
+<footer class="footer-distributed">
+  <div class="footer-left">
+    <h3><span>S2STORE</span></h3>
+    <p class="footer-links">
+      <a href="#" class="link-1">ĐỒNG HỒ NAM</a>
+
+      <a href="#">ĐỒNG HỒ NỮ</a>
+
+    </p>
+
+    <p class="footer-company-name">S2STORE © 2021</p>
+  </div>
+
+  <div class="footer-center">
+
+    <div>
+      <i class="fa fa-map-marker"></i>
+      <p><span>1 Cầu Giấy</span> Hà Nội, Việt Nam</p>
+    </div>
+
+    <div>
+      <i class="fa fa-phone"></i>
+      <p>+84.1412.1412.14</p>
+    </div>
+
+    <div>
+      <i class="fa fa-envelope"></i>
+      <p><a href="mailto:support@company.com">cskh@s2store.com</a></p>
+    </div>
+  </div>
+
+  <div class="footer-right">
+
+    <p class="footer-company-about">
+      <span>HANOI STORES</span>
+      33 Hàm Long, Hoàn Kiếm.<br>
+      9 B7 Phạm Ngọc Thạch, Đống Đa.<br>
+      173C Kim Mã, Ba Đình.
+    </p>
+
+    <div class="footer-icons">
+      <a href="#"><i class="fa fa-facebook"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <a href="#"><i class="fa fa-linkedin"></i></a>
+      <a href="#"><i class="fa fa-github"></i></a>
+    </div>
+
+  </div>
+</footer>
